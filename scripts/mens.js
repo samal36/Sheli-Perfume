8dc31c9a316b58372170bccb5eb2f2eedca8e4a7
samal36 <sjsamal36@gmail.com>
2023-11-17 03:08:53 -0800

    ADDED NEW PHOTOS

--- scripts/mens.js
+++ scripts/mens.js
@@ -39,16 +39,20 @@
               var card = document.createElement('div');
               card.className = 'col';
               card.innerHTML = `
-                <div class="card" style="width: 18rem;">
-                    <img src="/Images/perfumes/${perfume.brand}/${perfume.name}.png" class="card-img-top img-fluid" style="height:15rem;" alt="...">
-                    <div class="card-body">
-                        <h4 class="card-title">${perfume.name}</h4>
-                        <h6 class="card-text">${perfume.brand}</h6>
-                        <p class="card-text text-decoration-line-through">$${perfume.price}</p>
-                        <p class="card-text text-center">Starting at <span class=" fs-4">${getDiscountedPrice(perfume, discounts)}</span></p>
-                    </div>
-                </div>
+                  <div class="card p-2 rounded-4" style="width: 18rem; transition: color 0.3s;">
+                      <img src="/Images/perfumes/${perfume.brand}/${perfume.name}.png" class="card-img-top img-fluid" style="height:15rem;" alt="Image of perfume ${perfume.name} by ${perfume.brand}">
+                      <div class="card-body">
+                          <h5 class="card-title " id="cardTitle">${perfume.name}</h5>
+                          <h6 class="card-text">${perfume.brand}</h6>
+                          <p class="card-text text-decoration-line-through">$${perfume.price}</p>
+                          <p class="card-text text-center">Starting at <span class="fs-4">${getDiscountedPrice(perfume, discounts)}</span></p>
+                      </div>
+                  </div>
               `;
+              
+              // Append the perfume card to the container
+              perfumeContainer.appendChild(card);
+              
     
               // Append the perfume card to the container
               perfumeContainer.appendChild(card);
